ef17909fe229b30ff640e8aa79c1f3791120ce64
crashRT <crashrt.farbauti@gmail.com>
2024-05-05 07:20:21 +0900

    コメントを追加

--- app.py
+++ app.py
@@ -9,30 +9,39 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
 
-# ----------- 動かすための準備 -----------
-# ※ここは変更しないでください
+# ⚠️ここは変更しないでください
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 client = WebClient(token=os.getenv("BOT_TOKEN"), ssl=ssl_context)
 app = App(token=os.environ.get("BOT_TOKEN"), client=client)
-# ----------- 動かすための準備 -----------
+# ----------- 動かすための準備 ここまで -----------
 
 
+# "やっほー"というメッセージに対して"やっほー！！！"と返します
 @app.message("やっほー")
+# 👆👆 "やっほー"の部分を変えると、変更後のメッセージに対して返信します
 def message_echo(message, say):
     say("やっほー！！！")
+    # 👆👆 ここで返信するメッセージを決めています
+    # この場合、"やっほー！！！"と返信します
 
 
+# 「こだま ○○」に対して「○○」と返します
 @app.message(re.compile("こだま (.*)"))
+# 👆👆 (.*) の部分にはどんな文章でも入ります
 def message_(say, context):
     text = context["matches"][0]
+    # 👆👆 こうすると「こだま ○○」の○○の部分を取り出せます
     say(text)
 
 
-# ----------- 動かすための準備 -----------
-# ここも変更しないでください
+# ここから下に新しい処理を追加していってください
+# いろんな処理を追加して，SlackBotをカスタマイズしていきましょう！
+
+
+# ----------- 動かすための準備2 ここから -----------
+# ⚠️ここも変更しないでください
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ.get("APP_TOKEN"))
     handler.start()
-# ----------- 動かすための準備 -----------
